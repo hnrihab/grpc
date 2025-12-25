5bab8788b8fc68e2beec4bb26530e7a4ea4cdfd7
rihab <rihabh18@gmail.com>
2025-12-25 06:10:54 +0100

    commit

--- mediaserver/src/main/java/com/example/mediaserver/grpc/VideoGrpcService.java
+++ mediaserver/src/main/java/com/example/mediaserver/grpc/VideoGrpcService.java
@@ -1,12 +1,19 @@
 package com.example.mediaserver.grpc;
 
+import com.example.Video;
+import com.example.VideoIdRequest;
 import com.example.UploadVideoRequest;
 import com.example.VideoServiceGrpc;
+
+
+import com.example.mediaserver.entities.Creator;
+import com.example.mediaserver.entities.VideoEntity; // JPAimport com.example.mediaserver.entities.VideoEntity;
 import com.example.mediaserver.mappers.VideoMapper;
-import com.example.mediaserver.repositories.CreatorRepository;
 import com.example.mediaserver.repositories.VideoRepository;
+import com.example.mediaserver.repositories.CreatorRepository;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+
 @GrpcService
 public class VideoGrpcService extends VideoServiceGrpc.VideoServiceImplBase {
 
@@ -25,23 +32,30 @@ public class VideoGrpcService extends VideoServiceGrpc.VideoServiceImplBase {
     }
 
     @Override
-    public void uploadVideo(UploadVideoRequest request,
-                            StreamObserver<com.example.Video> responseObserver) {
+    public void uploadVideo(UploadVideoRequest request, StreamObserver<Video> responseObserver) {
+        var creator = request.getCreator().getId().isEmpty()
+                ? creatorRepository.save(Creator.builder()
+                .name(request.getCreator().getName())
+                .email(request.getCreator().getEmail())
+                .build())
+                : creatorRepository.findById(Long.parseLong(request.getCreator().getId()))
+                .orElseThrow(() -> new RuntimeException("Creator not found"));
 
-        // 1️⃣ récupérer le creator
-        var creator = creatorRepository.findById(
-                Long.parseLong(request.getCreator().getId())
-        ).orElseThrow(() -> new RuntimeException("Creator not found"));
 
-        // 2️⃣ mapper request → entity
         var videoEntity = videoMapper.toEntity(request, creator);
-
-        // 3️⃣ save
         videoRepository.save(videoEntity);
 
-        // 4️⃣ mapper entity → proto
-        var response = videoMapper.toProto(videoEntity);
+        Video response = videoMapper.toProto(videoEntity);
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
+    @Override
+    public void getVideo(VideoIdRequest request, StreamObserver<Video> responseObserver) {
+        VideoEntity videoEntity = videoRepository.findById(Long.parseLong(request.getId()))
+                .orElseThrow(() -> new RuntimeException("Video not found"));
+
+        Video response = videoMapper.toProto(videoEntity);
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
